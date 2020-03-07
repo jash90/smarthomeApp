8523175fb451942b3f5103e900fec30c4abc786a
Bartłomiej Zimny <bartlomiejzimny@outlook.com>
2020-03-07 13:09:03 +0100

    change controls

--- src/components/ControlSlider.tsx
+++ src/components/ControlSlider.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { State, TouchableOpacity } from "react-native-gesture-handler";
-import { View } from "react-native";
+import { View, ActivityIndicator } from "react-native";
 import { Control, ControlText, H4 } from "./StyledComponent";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import Slider from "@react-native-community/slider";
@@ -11,40 +11,50 @@ interface Props {
     value: number;
     min: number;
     max: number;
+    loading: boolean;
     onValueChange?: any;
 }
 
 export default class ControlSlider extends Component<Props> {
     render() {
         return (
-            <View style={{ flexDirection: "row", alignItems:"center" }}>
+            <View style={{ flexDirection: "row", alignItems: "center" }}>
                 <Control>
-                    <Icon name={this.props.icon} size={40} color={"#D0DBE8"} />
+                    <Icon name={this.props.icon} size={40} color={"#FF7500"} />
                 </Control>
 
-                <View style={{flexDirection:"column", flex:1}}>
+                <View style={{ flexDirection: "column", flex: 1 }}>
                     <ControlText
                         style={{
-                           width:"100%",
+                            width: "100%",
                             alignSelf: "center",
                             color: "#D0DBE8",
-                            textAlign:"center"
+                            textAlign: "center"
                         }}>
                         {this.props.name}
                     </ControlText>
 
                     <View style={{ flexDirection: "row" }}>
-                        <Slider
-                            style={{ flex: 1, height: 40 }}
-                            minimumValue={this.props.min}
-                            maximumValue={this.props.max}
-                            step={1}
-                            value={this.props.value}
-                            thumbTintColor="#FF7500"
-                            minimumTrackTintColor="#FF7500"
-                            maximumTrackTintColor="#D0DBE8"
-                            onValueChange={this.props.onValueChange}
-                        />
+                        {this.props.loading ? (
+                            <View style={{ flex: 1 }}>
+                                <ActivityIndicator
+                                    size={40}
+                                    color={"#FF7500"}
+                                />
+                            </View>
+                        ) : (
+                            <Slider
+                                style={{ flex: 1, height: 40 }}
+                                minimumValue={this.props.min}
+                                maximumValue={this.props.max}
+                                step={1}
+                                value={this.props.value}
+                                thumbTintColor="#FF7500"
+                                minimumTrackTintColor="#FF7500"
+                                maximumTrackTintColor="#D0DBE8"
+                                onSlidingComplete={this.props.onValueChange}
+                            />
+                        )}
                         <H4 style={{ marginHorizontal: 5 }}>
                             {this.props.value}
                         </H4>
--- src/components/ControlSwitch.tsx
+++ src/components/ControlSwitch.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { State, TouchableOpacity } from "react-native-gesture-handler";
-import { View } from "react-native";
+import { View, ActivityIndicator } from "react-native";
 import { Control, ControlText } from "./StyledComponent";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
@@ -8,7 +8,8 @@ interface Props {
     icon: string;
     name: string;
     value: boolean;
-    onPress?: () => {};
+    loading: boolean;
+    onPress?: any;
 }
 
 export default class ControlSwitch extends Component<Props> {
@@ -17,15 +18,19 @@ export default class ControlSwitch extends Component<Props> {
             <TouchableOpacity onPress={() => this.props.onPress}>
                 <View style={{ flexDirection: "row" }}>
                     <Control>
-                        <Icon
-                            name={this.props.icon}
-                            size={40}
-                            color={this.props.value ? "#FF7500" : "#D0DBE8"}
-                        />
+                        {this.props.loading ? (
+                            <ActivityIndicator size={40} color={"#D0DBE8"} />
+                        ) : (
+                            <Icon
+                                name={this.props.icon}
+                                size={40}
+                                color={this.props.value ? "#FF7500" : "#D0DBE8"}
+                            />
+                        )}
                     </Control>
                     <ControlText
                         style={{
-                            alignSelf:"center",
+                            alignSelf: "center",
                             marginLeft: 20,
                             color: this.props.value ? "#FF7500" : "#D0DBE8"
                         }}>
